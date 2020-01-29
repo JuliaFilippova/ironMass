6fb0ea19dfdc524bb83d9e11cb16cb5bc82243c5
Julia Filippova <mega.yulia1996@mail.ru>
2020-01-30 02:16:49 +0300

    +parallax

--- js/main.js
+++ js/main.js
@@ -7,17 +7,34 @@ $('.about-slider').slick({
     cssEase: 'ease-in-out',
     touchThreshold: 100
 })
+// scroll animation 
+ AOS.init({
+ 	disable: 'phone',
+ });
 
-$(function() {
+
+
+// $(function() {
 	
-	// $(".scroll-panel").css({
-	// 	"height":$(window).height()
-	// });
+// 	$(".scroll-panel").css({
+// 		"height":$(window).height()
+// 	});
 
-	$.scrollify({
-		section:".scroll-panel",
-	    scrollSpeed: 1100,
-	    offset : 0, // расстояние в пикселях для комппенсации положения каждого раздела.
-	    scrollbars: false //Будет ли видна полоса прокрутки
-	});
-});
+// 	$.scrollify({
+// 		section:".scroll-panel",
+// 	    scrollSpeed: 1100,
+// 	    offset : 0, // расстояние в пикселях для комппенсации положения каждого раздела.
+// 	    scrollbars: false //Будет ли видна полоса прокрутки
+// 	});
+// });
+
+
+
+// PARALLAX
+ // const parallax = document.getElementById ("parallax");
+ // window.addEventListener("scroll", function () {
+ // 	let offset = window.pageYOffset;
+ // 	// console.log ( 'Offset:' + offset);
+ // 	// console.log ( 'Offset * 0.7' + offset);
+ // 	parallax.style.backgroundPositionY = offset * 0.7 + "px";
+ // })
