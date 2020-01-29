98353673fa08d4c14535b9bdaa7de6841d581408
Julia Filippova <mega.yulia1996@mail.ru>
2020-01-29 19:27:35 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+} )
+
 // slick-slider
 $('.about-slider').slick({
     draggable: true,
@@ -6,8 +15,4 @@ $('.about-slider').slick({
     infinite: false,
     cssEase: 'ease-in-out',
     touchThreshold: 100
-})
-
-$(function() {
-	jQuery.scrollSpeed(100, 800, 'easeOutCubic');
 })
